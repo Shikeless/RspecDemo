92a2c335b2227809b2001d290073fd607e115981
Shikeless <Shikeless@gmail.com>
2019-04-29 20:59:49 +0300

    addig new testings

---
+++ car_spec.rb
@@ -0,0 +1,21 @@
+require './car'
+
+describe Car do
+
+	# arrange
+	before do
+		@car = Car.new
+	end
+
+	it "must return range" do
+		# act
+
+		@car.add_fuel 10
+
+		# assert
+
+		expect(@car.range).to eq 200
+
+	end
+
+end
